780d3855f3487bf163a1392e0f233f4680733985
amorg025 <amorg025@odu.edu>
2023-09-20 23:56:33 -0400

    completed assignment

---
+++ webserver.py
@@ -0,0 +1,42 @@
+#import socket module
+from socket import *
+import sys # In order to terminate the program
+
+# Create a server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepare a server socket
+serverPort = 6789  #
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)  
+
+print(f"The server is ready to receive connections on port {serverPort}")
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+
+    try:
+        message = connectionSocket.recv(1024).decode()  # Receive the HTTP request message
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+        
+        # Send HTTP headers into the socket
+        response_headers = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+        connectionSocket.send(response_headers.encode())
+
+        # Send the content of the requested file to the client
+        connectionSocket.send(outputdata.encode())
+
+        connectionSocket.close()
+    except IOError:
+        # Send a 404 Not Found response message
+        not_found_response = "HTTP/1.1 404 Not Found\r\n\r\n<h1>404 Not Found</h1>"
+        connectionSocket.send(not_found_response.encode())
+        connectionSocket.close()
+
+# Close the server socket
+serverSocket.close()
+sys.exit()  # Terminate the program after serving
